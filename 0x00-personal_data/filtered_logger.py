71c94ad6eb7415e65d6ae4ee1abc2d6f482f5738
Idris Adebowale <idrys01@gmail.com>
2024-01-11 11:33:31 +0000

    add Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -18,3 +18,22 @@ def filter_datum(
     n = fd("".join([pat.format(fi, fi, separator) for fi in fields]), message)
     va = "" if not n else "|".join(["=" + i for i in n[0]])
     return message if not fields else re.sub(va, "=" + redaction, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format for the log message"""
+        record.msg = filter_datum(
+                self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super().format(record)
