75654cb2b4a9087d7350ffad101a8f53b0350d5e
Idris Adebowale <idrys01@gmail.com>
2024-01-13 17:28:28 +0000

    change return type to bytes

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """This module define a function that hash a password
 """
-from typing import ByteString
 import bcrypt
 
 
-def hash_password(password: str) -> ByteString:
+def hash_password(password: str) -> bytes:
     """hash a given password string
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
