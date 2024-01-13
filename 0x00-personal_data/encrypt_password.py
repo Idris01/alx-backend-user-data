2e55da64d836ef14f98e8e7c150a6c5b6adfa674
Idris Adebowale <idrys01@gmail.com>
2024-01-13 17:36:24 +0000

    add is_valid function that checks if a password string matches hashed_password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,12 @@ def hash_password(password: str) -> bytes:
     """hash a given password string
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(
+        hashed_password: bytes,
+        password: str) -> bool:
+    """check if a given password matches that hash"""
+    return bcrypt.checkpw(
+            password.encode('utf-8'),
+            hashed_password)
