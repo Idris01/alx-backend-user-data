3316371dca3196282836517f38bfbec0c45ed018
Idris Adebowale <idrys01@gmail.com>
2024-01-15 16:21:59 +0000

    add method that extract user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,7 +8,8 @@ import base64
 class BasicAuth(Auth):
     """The Basic Authentication class
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """Extract base64 auth from the auth header
         """
         auth_hdr = authorization_header
@@ -18,7 +19,8 @@ class BasicAuth(Auth):
             return None
         return auth_hdr[auth_hdr.index(" ") + 1:]
 
-    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
         """Decode the base64 auth header"""
         b64_auth = base64_authorization_header
         if b64_auth is None or not isinstance(b64_auth, str):
@@ -27,3 +29,14 @@ class BasicAuth(Auth):
             return base64.b64decode(b64_auth).decode("utf-8")
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str) -> (str, str):
+        """Extract the user credentials from decoded auth header
+        """
+        dec_b64 = decoded_base64_authorization_header
+        if not dec_b64 or not isinstance(dec_b64, str) or ":" not in dec_b64:
+            return (None, None)
+        colon_index = dec_b64.index(":")
+        return (dec_b64[:colon_index], dec_b64[colon_index + 1:])
