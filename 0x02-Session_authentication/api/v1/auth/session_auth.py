4e723c3ff6795d44f06da191653053b72c369766
Idris Adebowale <idrys01@gmail.com>
2024-01-18 08:40:35 +0000

    add SessionAuth class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module define the Session Auth class
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """This class define the Session Authentication
+    """
+    pass
