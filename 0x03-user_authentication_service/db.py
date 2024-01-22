b22f7b7ff3102b1f6e8180a551ab995c8cf5fa51
Idris Adebowale <idrys01@gmail.com>
2024-01-22 11:50:07 +0000

    add get_user_by method to DB class, This method finds user based on arbitrary arguments

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,7 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -15,7 +16,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -43,3 +44,24 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user in the database based on the given criteria
+
+        Args:
+            **kwargs: Arbitrary keyword arguments for filtering
+
+        Returns:
+            User: First user found based on the provided criteria
+
+        Raises:
+            NoResultFound: If no result is found
+            InvalidRequestError: If an invalid query argument is passed
+        """
+        invalid_args = set(kwargs) - set(User.__table__.columns.keys())
+        if invalid_args:
+            raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        return user
