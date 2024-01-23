c05da133c2ce9b92a15df8bf6a93032854300a0e
Idris Adebowale <idrys01@gmail.com>
2024-01-23 15:02:25 +0000

    add basic flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Flask app module
+"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """Define the home route
+    """
+    return jsonify(dict(message="I am a Software Engineer"))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
