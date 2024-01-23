88324844b7b831c8dcfe4ec8a25a40725e4f0f5a
Idris Adebowale <idrys01@gmail.com>
2024-01-23 20:52:12 +0000

    add users url

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 """Flask app module
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -11,6 +16,19 @@ def home():
     """Define the home route
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def users():
+    """Handles user creation
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify(dict(email=email, message="user created"))
+    except ValueError as e:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
