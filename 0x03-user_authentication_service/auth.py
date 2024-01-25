cbfed6a01db97414bf21e20f4aa05df90992c759
Idris Adebowale <idrys01@gmail.com>
2024-01-25 16:51:47 +0000

    add update_password method to Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -125,3 +125,14 @@ class Auth:
             return token
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Reset password of a user
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            user.hashed_password = _hash_password(password)
+            user.reset_token = None
+        except NoResultFound:
+            raise ValueError
+        return None
